cac9b43cf3ac1a39db0174cefe65c351dd9504cb
Alexs <ale.osantos@hotmail.com>
2025-02-21 17:21:24 +0000

    feat: product header component

---
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import type { Product, Restaurant } from "@prisma/client";
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+interface ProductHeaderProps {
+  product: Pick<Product, "imageUrl" | "name">;
+}
+
+const ProductHeader = ({ product }: ProductHeaderProps) => {
+  const router = useRouter();
+  const handleBackClick = () => router.back();
+
+  return (
+    <div className="relative h-[300px] w-full">
+      <Button
+        size="icon"
+        variant="secondary"
+        className="absolute left-4 top-4 z-50 rounded-full"
+        onClick={handleBackClick}
+      >
+        <ChevronLeftIcon />
+      </Button>
+
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        fill
+        className="object-contain"
+        priority
+      />
+      <Button
+        size="icon"
+        variant="secondary"
+        className="absolute right-4 top-4 z-50 rounded-full"
+      >
+        <ScrollTextIcon />
+      </Button>
+    </div>
+  );
+};
+
+export default ProductHeader;
